742adebca61cd5cc3a144e033990670bb58d7324
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-04-04 03:01:55 +0300

    added button to clear input

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,6 @@
 import { useId } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { IoIosClose } from 'react-icons/io';
 
 import css from './SearchBox.module.css';
 import { changeFilter } from '../../redux/filters/slice';
@@ -13,15 +14,24 @@ export const SearchBox = () => {
   return (
     <div className={css.searchbox}>
       <label htmlFor={SearchBoxId}>Find contacts by name</label>
-      <input
-        id={SearchBoxId}
-        name="SearchBox"
-        type="text"
-        placeholder="Enter contact name for search"
-        value={value}
-        onChange={(evt) => dispatch(changeFilter(evt.target.value))}
-        className={css.input}
-      />
+      <div className={css.inputWrap}>
+        <input
+          id={SearchBoxId}
+          name="SearchBox"
+          type="text"
+          placeholder="Enter contact name for search"
+          value={value}
+          onChange={(evt) => dispatch(changeFilter(evt.target.value))}
+          className={css.input}
+        />
+        <button
+          type="button"
+          className={css.btn}
+          onClick={() => dispatch(changeFilter(''))}
+        >
+          <IoIosClose size="30" />
+        </button>
+      </div>
     </div>
   );
 };
